a70408cdd262e3b04540b461ba0e01553d569f75
Chloekkk <skdusdl8804@gmail.com>
2023-01-28 21:07:09 +0900

    🚨 test: Update cpp googletest and cmake files
    #

---
+++ test/iterator.cpp
@@ -0,0 +1,18 @@
+/*
+ * File: iterator.cpp
+ * Project: ft_containers
+ * Created Date: 2023/01/28
+ * Author: nkim
+ * Copyright (c) 2022 nkim
+ */
+
+#include "gtest/gtest.h"
+
+namespace {
+TEST(config_test, allow_method) {
+  EXPECT_EQ(false, false);
+}
+}
+
+
+
---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: main.cpp
+ * Project: ft_container
+ * Created Date: 2023/01/28
+ * Author: nkim
+ * Copyright (c) 2022 nkim
+ */
+
+#include "gtest/gtest.h"
+
+int main(int ac, char **av) {
+  ::testing::InitGoogleTest(&ac, av);
+  return RUN_ALL_TESTS();
+}
