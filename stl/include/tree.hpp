79c83bce1606c882a74aa4996bcddd3be350bd97
Chloekkk <skdusdl8804@gmail.com>
2023-02-20 23:10:28 +0900

    ✨ feat: Update rb_tree_iterator
    #

--- stl/include/tree.hpp
+++ stl/include/tree.hpp
@@ -9,6 +9,7 @@
 #ifndef TREE_HPP_
 #define TREE_HPP_
 
+#include "iterator_traits.hpp"
 #include <memory>
 
 // 나중에 지워야 함
@@ -16,6 +17,9 @@
 
 namespace ft {
 
+/**
+ * @brief rb_tree color constant
+ */
 enum _rb_tree_color {
   _s_red = false,
   _s_black = true
@@ -55,7 +59,7 @@ struct _rb_tree_node_base {
 };
 
 template<class Val>
-struct _rb_tree_node {
+struct _rb_tree_node : public _rb_tree_node_base {
   typedef _rb_tree_node<Val> *_link_type;
 
   Val _m_value_field;
@@ -72,7 +76,7 @@ struct _rb_tree_header {
   _rb_tree_node_base _m_header;
   size_t _m_node_count; // keeps track of size of tree
 
-  _rb_tree_header() {
+  _rb_tree_header() : _m_header(), _m_node_count() {
     _m_header._m_color = _s_red;
     _m_reset();
   }
@@ -103,6 +107,80 @@ class _rb_tree_impl {
 
   // todo: _m_put_node 무슨 함수인지 분석해야함
   void _m_put_node(_rb_tree_node<T> *p) { _m_node_allocator.deallocate(p, 1); }
+};
+
+// ft rb_tree static function
+_rb_tree_node_base *
+_rb_tree_increment(_rb_tree_node_base *x) throw();
+
+const _rb_tree_node_base *
+_rb_tree_increment(const _rb_tree_node_base *x) throw();
+
+_rb_tree_node_base *
+_rb_tree_decrement(_rb_tree_node_base *x) throw();
+
+const _rb_tree_node_base *
+_rb_tree_decrement(const _rb_tree_node_base *x) throw();
+
+template<typename T>
+struct _rb_tree_iterator : public std::iterator<std::bidirectional_iterator_tag,
+                                                typename iterator_traits<T>::value_type,
+                                                typename iterator_traits<T>::difference_type,
+                                                typename iterator_traits<T>::pointer,
+                                                typename iterator_traits<T>::reference> {
+  typedef _rb_tree_iterator<T> iterator;
+  typedef typename iterator_traits<T>::difference_type difference_type;
+  typedef typename iterator_traits<T>::reference reference;
+  typedef typename iterator_traits<T>::pointer pointer;
+  typedef typename iterator_traits<T>::value_type value_type;
+  typedef std::bidirectional_iterator_tag iterator_category;
+
+  typedef _rb_tree_iterator<T> _self;
+  typedef _rb_tree_node_base::_base_ptr _base_ptr;
+  typedef _rb_tree_node<T> *_link_type;
+
+  _base_ptr _m_node;
+
+  _rb_tree_iterator() : _m_node() {}
+  _rb_tree_iterator(_base_ptr x) : _m_node(x) {}
+  _rb_tree_iterator(const iterator &it) : _m_node(it._m_node) {}
+
+  // const rb_iterator to non-const rb_iterator
+  iterator _m_const_cast() const {
+    return iterator(const_cast<typename iterator::_base_ptr>(_m_node));
+  }
+
+  pointer operator->() const {
+    return static_cast<_link_type>(_m_node)->_m_valptr();
+  } // node->var
+  reference operator*() const {
+    return *static_cast<_link_type>(_m_node)->_m_valptr();
+  } // *node
+  _self &operator++() {
+    _m_node = _rb_tree_increment(_m_node);
+    return *this;
+  } // ++node
+  _self operator++(int) {
+    _self _tmp = *this;
+    _m_node = _rb_tree_increment(_m_node);
+    return _tmp;
+  } // node++
+  _self &operator--() {
+    _m_node = _rb_tree_decrement(_m_node);
+    return *this;
+  } // --node
+  _self operator--(int) {
+    _self _tmp = *this;
+    _m_node = _rb_tree_decrement(_m_node);
+    return _tmp;
+  } // node--
+
+  friend bool operator==(const _self &x, const _self &y) {
+    return x._m_node == y._m_node;
+  }
+  friend bool operator!=(const _self &x, const _self &y) {
+    return x._m_node != y._m_node;
+  }
 };
 
 template<class Key, class Val, class KeyOfValue, class Compare, class Alloc = std::allocator<Val> >
---
+++ test/map_test.cpp
@@ -0,0 +1,7 @@
+/*
+ * File: map_test.cpp
+ * Project: ft_container
+ * Created Date: 2023/02/20
+ * Author: nkim
+ * Copyright (c) 2022 nkim
+ */
