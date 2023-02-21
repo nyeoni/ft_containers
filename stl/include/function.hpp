c51dd86c9a72386a0682e53d983ae786dc6b6083
Chloekkk <skdusdl8804@gmail.com>
2023-02-21 22:25:31 +0900

    ✨ feat: Update function.hpp binary_function template structure
    #

--- stl/include/function.hpp
+++ stl/include/function.hpp
@@ -17,6 +17,18 @@ struct unary_function {
   typedef Result result_type;
 };
 
+template<typename Arg1, typename Arg2, typename Result>
+struct binary_function {
+  typedef Arg1 first_argument_type;
+  typedef Arg2 second_argumnent_type;
+  typedef Result result_type;
+};
+
+template<class T>
+struct less : binary_function<T, T, bool> {
+  bool operator()(const T &x, const T &y) const { return x < y; }
+};
+
 template<class Pair>
 struct Select1st : public unary_function<Pair, typename Pair::first_type> {
   typename Pair::first_type &operator()(Pair &x) const {
